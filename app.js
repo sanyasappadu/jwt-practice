4dd27157a497dcc7b914b677abf2599e2eecad3e
sanyasappadu <sanyasappadu4@gmail.com>
2024-03-25 20:24:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000 
+
+app.listen(PORT, ()=>console.log(`server running on port ${PORT}`));
