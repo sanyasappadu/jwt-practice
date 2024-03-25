af79c07bb18d9076eeafb5a3710c39c2744fe305
sanyasappadu <sanyasappadu4@gmail.com>
2024-03-25 21:11:17 +0530

    basic auth routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const createError = require('http-errors');
+const authRoutes = require('./Routes/Auth.route')
 require('dotenv').config();
 
 const app = express();
@@ -10,13 +11,14 @@ app.get('/', async(req, res, next)=>{
 res.send("hello from express");
 });
 
+app.use('/auth', authRoutes);
 app.use(async (req, res, next) => {
   // const error = new Error('Not fount');
   // error.status = 404
   // next(error);
 
   // next(createError.NotFound())
-  
+
   next(createError.NotFound('this route does not exist'))
 })
 app.use((err, req, res, next)=>{
